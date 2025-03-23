16d2e1877105230f1150330ee2acab87ffc28f8b
romin014 <sohyun014@naver.com>
2025-03-23 16:41:04 +0900

    Schedule project start

---
+++ src/main/java/com/example/boot_schedule_management/BootScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot_schedule_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootScheduleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootScheduleManagementApplication.class, args);
+    }
+
+}
